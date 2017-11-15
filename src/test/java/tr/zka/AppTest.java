9ae518913add135f8e822f5704cfdc08141af3d1
Your Name <karatatarzeynep@gmail.com>
2017-11-16 02:30:56 +0300

    unit test

--- src/test/java/tr/zka/AppTest.java
+++ src/test/java/tr/zka/AppTest.java
@@ -1,38 +1,36 @@
 package tr.zka;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import tr.zka.cheksum.impl.Md5Checksum;
+import tr.zka.validator.impl.LinkValidator;
+
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    private String content = "Hello World";
+    private String checksum = "sQqNsWTgdUEFt6mb5y4/5Q==";
+
+    private String url = "https://stackoverflow.com/questions/6119392/junit-testing-for-a-boolean-method";
+
+    public void testChecksum() throws NoSuchAlgorithmException {
+        Md5Checksum md5Checksum = new Md5Checksum();
+
+        String generatedChecksum = md5Checksum.computeCheksum(content);
+
+        assertTrue(checksum.equals(generatedChecksum) || generatedChecksum.equals(checksum));
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testLinkValidator() {
+        boolean isValid = new LinkValidator().isValidLink(url);
+        assertEquals(isValid, true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+
 }
